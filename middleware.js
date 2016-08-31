fc08ba4e32f0b85650e9f7587af12b68f0ac20b3
Christopher <chriscrowd@gmail.com>
2016-08-31 14:58:49 -0400

    seperated middleware from server config

---
+++ middleware.js
@@ -0,0 +1,12 @@
+module.exports  = {
+          requireAuth:function(req,res,next) {
+             console.log('private route hit');
+
+              next();
+          },
+          logger:function(req,res,next) {
+            let log = new Date().toString();
+            console.log(`Request: ${req.method} ${req.originalUrl} created ${log}`);
+            next();
+          }
+        }
--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 var express = require('express');
  var app = express();
+ var middleware = require('./middleware');
 
-var  middleware = {
-  requireAuth:function(req,res,next) {
-     console.log('private route hit');
 
-      next();
-  },
-  logger:function(req,res,next) {
-    let log = new Date().toString();
-    console.log(`Request: ${req.method} ${req.originalUrl} created ${log}`);
-    next();
-  }
-}
 app.use(middleware.logger);
  app.get('/about',middleware.requireAuth,function(req,res) {
     res.send('About me!');
